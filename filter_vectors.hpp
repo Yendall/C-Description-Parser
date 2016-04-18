c42912ce6b021e5ac4b7b0aba2ca004ad2063514
Yendall <yendallmax@gmail.com>
2016-04-18 18:53:48 +1000

    Filter Vectors update

--- filter_vectors.hpp
+++ filter_vectors.hpp
@@ -16,14 +16,31 @@ class Filter_Vectors
 		~Filter_Vectors() {};
 		
 		// Filter variables
-		std::vector < STR > irrelevancy_vector;
-		std::vector < STR > uniqueness_vector;
+		std::vector <STR> 	irrelevancy_vector;
+		std::vector <STR> 	uniqueness_vector;
+		std::vector <STR>	top_100_words;
+		// Keyword dictionaries
+		KEY_VAL_FIELDS 		keyword_map;
+		
 		
 		// Filter functions
-		bool tokenise_description(const std::string&);
-		std::string filter_irrelevancy(const CSV_FIELDS&);
-		
-		
-		
+		void 			init_description_parsing();
+		void			set_top100_vector(CSV_FIELDS&);
+		void			set_keyword_map(KEY_VAL_FIELDS&);
+		bool			parse_keyword_vectors(const CSV_FIELDS&);
+		bool 			tokenise_description(const std::string&);		
+		STR 			filter_irrelevancy(const CSV_FIELDS&);	
+		CSV_FIELDS		parse_csv_file(STR);
 };
+
+// Keyword map setter
+void Filter_Vectors::set_keyword_map(KEY_VAL_FIELDS& map)
+{
+	keyword_map = map;
+}
+
+void Filter_Vectors::set_top100_vector(CSV_FIELDS& vector)
+{
+	top_100_words = vector;
+}
 #endif
