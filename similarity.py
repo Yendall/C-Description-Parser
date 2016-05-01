128071e39eb150985952e2531b9b1e6469e6af48
Yendall <yendallmax@gmail.com>
2016-05-01 20:16:25 +1000

    Similarity Python Changed

--- similarity.py
+++ similarity.py
@@ -4,12 +4,6 @@ import scipy
 import glob
 
 # Filenames for training a corpus
-# filenames = ['data/austen-bronte/Austen_Emma.txt',
-#             'data/austen-bronte/Austen_Pride.txt',
-#             'data/austen-bronte/Austen_Sense.txt',
-#             'data/austen-bronte/CBronte_Jane.txt',
-#             'data/austen-bronte/CBronte_Professor.txt',
-#             'data/austen-bronte/CBronte_Villette.txt']
 filenames = glob.glob('data/idv_data/title_set/*.txt')
 
 
@@ -31,7 +25,7 @@ house_idx = list(vocab).index('house')
 print dtm[0, house_idx]
 
 # using NumPy indexing will be more natural for many
-# in R this would be essentially the same, dtm[1, vocab == 'house']
+# in R this would be essentially the same
 print dtm[0, vocab == 'house']
 
 # "by hand"
@@ -51,28 +45,11 @@ dist = euclidean_distances(dtm)
 
 np.round(dist, 1)
 
-# *Pride and Prejudice* is index 1 and *Jane Eyre* is index 3
-filenames[1] == 'data/austen-bronte/Austen_Pride.txt'
-filenames[3] == 'data/austen-bronte/CBronte_Jane.txt'
-# the distance between *Pride and Prejudice* and *Jane Eyre*
-print dist[1, 3]
-
-# which is greater than the distance between *Jane Eyre* and *Villette* (index 5)
-print dist[1, 3] > dist[3, 5]
-
 from sklearn.metrics.pairwise import cosine_similarity
 
 dist = 1 - cosine_similarity(dtm)
 
 np.round(dist, 2)
-
-# the distance between *Pride and Prejudice* (index 1)
-# and *Jane Eyre* (index 3) is
-dist[1, 3]
-
-# which is greater than the distance between *Jane Eyre* and
-# *Villette* (index 5)
-dist[1, 3] > dist[3, 5]
 
 norms = np.sqrt(np.sum(dtm * dtm, axis=1, keepdims=True))  # multiplication between arrays is element-wise
 
@@ -81,8 +58,6 @@ dtm_normed = dtm / norms
 similarities = np.dot(dtm_normed, dtm_normed.T)
 
 np.round(similarities, 2)
-# similarities between *Pride and Prejudice* and *Jane Eyre* is
-print similarities[1, 3]
 
 import os  # for os.path.basename
 
@@ -100,7 +75,6 @@ pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
 xs, ys = pos[:, 0], pos[:, 1]
 
 # short versions of filenames:
-# convert 'data/austen-bronte/Austen_Emma.txt' to 'Austen_Emma'
 names = [os.path.basename(fn).replace('.txt', '') for fn in filenames]
 
 import os  # for os.path.basename
@@ -116,7 +90,6 @@ pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
 xs, ys = pos[:, 0], pos[:, 1]
 
 # short versions of filenames:
-# convert 'data/austen-bronte/Austen_Emma.txt' to 'Austen_Emma'
 names = [os.path.basename(fn).replace('.txt', '') for fn in filenames]
 
 # color-blind-friendly palette
